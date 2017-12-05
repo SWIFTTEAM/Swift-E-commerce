d7fa8d0be440522439143a37c9c8bc7519e0bba4
Abao <abao@AbaodeMacBook-Air.local>
2017-12-05 14:19:45 +0800

    yijing1205

---
+++ iOS/Ecommerce/Login/remember.swift
@@ -0,0 +1,31 @@
+//
+//  remember.swift
+//  Ecommerce
+//
+//  Created by Abao on 2017/11/20.
+//  Copyright © 2017年 Ecommerce. All rights reserved.
+//
+
+import UIKit
+
+public class remember: UIViewController {
+    
+   //一開始要先判斷有沒有登入過,自己放進資料庫
+   //先在sqlite建立資料庫與資料表
+   //在登入之後把帳密資料傳進去
+    
+    public func remembermyaccount(_ rememberBool: Bool) -> (ResultAccount: String , ResultPassword: String){
+    //先確認有沒有打勾記住密碼,有則動作,否則不動作
+        var account:String = "";
+        var password: String = "";
+    if(rememberBool == true){
+        
+    //有勾勾,就把資料庫儲存的account,password放上
+    }else{
+    //沒勾勾,account,password就清空
+    }
+    
+    
+    return(account,password);
+}
+}
