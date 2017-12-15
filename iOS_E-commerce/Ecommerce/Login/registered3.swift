721326b01ba2f2bc31fbc370bcab0470408ac3cc
ching <ching@chings-Mac.local>
2017-12-15 12:35:09 -0800

    xiuching1216

---
+++ iOS_E-commerce/Ecommerce/Login/registered3.swift
@@ -0,0 +1,13 @@
+//
+//  registered3.swift
+//  Ecommerce
+//
+//  Created by ching on 2017/12/15.
+//  Copyright © 2017年 Ecommerce. All rights reserved.
+//
+
+import UIKit
+
+class registered3: NSObject {
+
+}
