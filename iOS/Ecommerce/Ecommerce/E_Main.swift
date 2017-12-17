d28ee4c8eea4270cab75072a6cb429e7e213c728
黃仕杰 <huangshijie@huangshijiede-MacBook-Pro.local>
2017-12-17 13:49:08 +0800

    sj

--- iOS/Ecommerce/Ecommerce/E_Main.swift
+++ iOS/Ecommerce/Ecommerce/E_Main.swift
@@ -8,20 +8,33 @@
 
 import UIKit
 
-//-------------------------------------------------------------------------------
-struct Get_IP {
-    static let ip: String = "192.168.1.136";
+///==================================Get Value=============================================
+public class NSGetValue {
+    struct IP {
+        static let ip: String = "172.20.10.9";
+    }
+    
+    struct Php_Files {
+        static let login: String = "login.php";
+        static let register: String = "register.php";
+    }
 }
 
-//-------------------------------------------------------------------------------
-struct Get_Php_Files {
-    static let login: String = "login.php";
-    static let forget_password: String = "forget_password.php";
-}
-
-//-------------------------------------------------------------------------------
-/// NSPHP
+///==================================PHP=============================================
 class E_Main: PHPSQL{
     var postContent: String = "";
 };
+
+
+///==================================showMessage=============================================
+
+public func showMessage(UI: UIViewController, _ string: String) -> Void {
+    
+    let myAlert: UIAlertController = UIAlertController(title: "System Messgae", message: string, preferredStyle: .alert);
+    let action = UIAlertAction(title: "關閉", style: UIAlertActionStyle.default, handler: {action in print("done")});
+    myAlert.addAction(action);
+    UI.present(myAlert, animated: true, completion: nil);
+}
+
+
 
